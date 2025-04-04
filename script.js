79f47d033411cd7b4bc5e3e4817056794b943c1b
Alok Kumar <2784alok@gmail.com>
2025-04-05 00:38:38 +0530

    added functionaly upon clicking the image all details about plants with 3d-model appears

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
                 mobileMenu.classList.toggle('hidden');
             });
         });
+
+
+
+// for closing the image popup
+function openModal() {
+    document.getElementById("navbar").classList.add("hidden");
+    document.getElementById('plantModal1').classList.remove('hidden');
+}
+
+function closeModal() {
+    document.getElementById("navbar").classList.remove("hidden");
+    document.getElementById('plantModal1').classList.add('hidden');
+}
+
+
