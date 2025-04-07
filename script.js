e6ebc29166cd62fe14d20ff94630be5bfaf035c3
Alok Kumar <2784alok@gmail.com>
2025-04-07 12:09:50 +0530

    added some functionality

--- script.js
+++ script.js
@@ -10,14 +10,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 // for closing the image popup
-function openModal() {
+function openModal(modalId) {
     document.getElementById("navbar").classList.add("hidden");
-    document.getElementById('plantModal1').classList.remove('hidden');
+    document.getElementById(modalId).classList.remove("hidden");
 }
 
-function closeModal() {
+function closeModal(modalId) {
     document.getElementById("navbar").classList.remove("hidden");
-    document.getElementById('plantModal1').classList.add('hidden');
+    document.getElementById(modalId).classList.add("hidden");
 }
+
 
 
