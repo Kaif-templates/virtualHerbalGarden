24ee4e7e8f933a2d4c00ddf7794b1b603e9bec04
Gaurav Joshi <150048308+Gaurav1A2B@users.noreply.github.com>
2025-04-07 23:20:13 +0530

    Update admin.php
    correcting link

--- admin/admin.php
+++ admin/admin.php
@@ -35,7 +35,7 @@
             <a href="../index.html"><div class="hover:underline hover:text-green-500">Home</div></a>
             <a href="../login/login.php"><div class="hover:underline hover:text-green-500">Login</div></a>
             <a href="../Health/health.html"><div class="hover:underline hover:text-green-500">Health</div></a>
-            <a href="../Arun/community.php"><div class="hover:underline hover:text-green-500">Community</div></a>
+            <a href="../Dashboard/community.php"><div class="hover:underline hover:text-green-500">Community</div></a>
             <a href="../Dashboard/dashboard.php"><div class="hover:underline hover:text-green-500">Dashboard</div></a>
             <a href="./admin.php"><div class="hover:underline hover:text-green-500">Admin</div></a>
         </div>
@@ -53,7 +53,7 @@
             <a href="../index.html"><div class="p-4 hover:bg-gray-100 hover:text-green-500">Home</div></a>
             <a href="../login/login.php"><div class="p-4 hover:bg-gray-100 hover:text-green-500">Login</div></a>
             <a href="../Health/health.html"><div class="p-4 hover:bg-gray-100 hover:text-green-500">Health</div></a>
-            <a href="../Arun/community.php"><div class="p-4 hover:bg-gray-100 hover:text-green-500">Community</div></a>
+            <a href="../Dashboard/community.php"><div class="p-4 hover:bg-gray-100 hover:text-green-500">Community</div></a>
             <a href="../Dashboard/dashboard.php"><div class="p-4 hover:bg-gray-100 hover:text-green-500">Dashboard</div></a>
             <a href="./admin.php"><div class="p-4 hover:bg-gray-100 hover:text-green-500">Admin</div></a>
         </div>
@@ -91,7 +91,7 @@
                     <div><h2>Manage Herbs</h2></div>
                 </div>
             </a>
-            <a href="../Arun/community.php">
+            <a href="../Dashboard/community.php">
                 <div class="flex gap-4 hover:bg-green-600 hover:rounded-md pl-2 text-lg font-semibold pr-2 pt-2 pb-2">
                     <div><i class="fa-solid fa-comments"></i></div>
                     <div><h2>Community Forum</h2></div>
