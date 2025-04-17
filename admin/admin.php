0ea667059aae4faa2127f90ca2f5a7335997d941
Arun0041 <arungautam0041@gmail.com>
2025-04-17 20:27:46 +0530

    bg-fixed

--- admin/admin.php
+++ admin/admin.php
@@ -108,7 +108,7 @@
     </div>
 
     <!-- Main Content Area (Responsive Fix Applied) -->
-    <div class="bg-green-50 flex-1 p-10 w-full md:ml-64">
+    <div class=" flex-1 p-10 w-full md:ml-64">
         <div class="text-3xl">
             <h1 class="text-green-900 font-bold">Admin Dashboard, <?php echo htmlspecialchars($user_email); ?></h1>
         </div>
